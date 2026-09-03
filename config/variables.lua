67dbeb153fafa557f45c537e32a1adef6a83843f
PurpleSide705 <gaelplascencia55@gmail.com>
2026-09-03 16:08:25 -0600

    Configuración Hyprland CachyOS

---
+++ config/variables.lua
@@ -0,0 +1,16 @@
+-- Hyprland default apps
+
+TERMINAL     = "kitty"
+FILE_MANAGER = "thunar"
+BROWSER      = "brave"
+EDITOR       = "micro"
+CALCULATOR   = "gnome-calculator"
+
+-- Monitors
+MONITOR1 = ""
+MONITOR2 = ""
+MONITOR3 = ""
+PRIMARY_MONITOR = MONITOR1
+
+-- Workspaces
+NUM_WPM = 4 -- Number of workspaces per monitor (Max 10)
